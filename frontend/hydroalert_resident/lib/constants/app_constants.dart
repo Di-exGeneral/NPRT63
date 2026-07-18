77449aa33f921cba1774c48b5a5374787e75f0c4
Tlotliso Ledwaba <160078468+Di-exGeneral@users.noreply.github.com>
2026-07-18 21:33:16 +0200

    Update base URL to production environment

--- frontend/hydroalert_resident/lib/constants/app_constants.dart
+++ frontend/hydroalert_resident/lib/constants/app_constants.dart
@@ -1,4 +1,4 @@
 class AppConstants {
-  static const String baseUrl = 'http://13.245.102.195:8000';
+  static const String baseUrl = 'https://nprt63-production.up.railway.app';
   static const String appName = 'HydroAlert';
 }
